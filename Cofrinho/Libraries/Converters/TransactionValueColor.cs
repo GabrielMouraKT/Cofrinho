5c54f23f58ff90d3f4500d21fd39af27c8c2a348
Gabriel <gabrieljknd@gmail.com>
2023-03-26 11:27:56 -0300

    Cores dos valres Income e Expenses

---
+++ Cofrinho/Libraries/Converters/TransactionValueColor.cs
@@ -0,0 +1,39 @@
+﻿using Cofrinho.Models;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cofrinho.Libraries.Converters
+{
+    class TransactionValueColorConverter : IValueConverter
+
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            Transaction transaction = (Transaction)value;
+            if (transaction == null)
+            {
+                return Colors.Black;
+            }
+            if (transaction.Type == TransactionType.Income)
+            {
+                return Colors.Green;
+
+            }
+            else
+            {
+                return Color.FromArgb("#FFff0000");
+            }
+
+            throw new NotImplementedException();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
